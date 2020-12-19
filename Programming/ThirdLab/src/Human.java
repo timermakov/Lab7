d649d1ec3119295d06f0867097069f407f54976e
Timofey Ermakov <ts.ermakov@yandex.ru>
2020-12-19 10:34:51 +0300

    UPD

--- Programming/ThirdLab/src/Human.java
+++ Programming/ThirdLab/src/Human.java
@@ -30,4 +30,103 @@ public abstract class Human {
     public int hashCode() {
         return Objects.hash(Name);
     }
+
+    public class Legs {
+        private String name;
+        private Human human;
+        private EricEricsonImpl EricEricson;
+        private CarlssonImpl Carlsson;
+
+        public Legs(String name, Human human) {
+            this.name = name;
+            this.human = human;
+            EricEricson = new EricEricsonImpl("его ");
+            Carlsson = new CarlssonImpl(" Карлсона");
+            this.setName(name, human);
+        }
+
+        public void setName(String name, Human human) {
+            if (human.equals(EricEricson)) {
+                this.name = EricEricson.getName() + name;
+            }
+            else if (human.equals(Carlsson)) {
+                this.name = name + Carlsson.getName();
+            }
+        }
+        public String getName() {
+            return this.name;
+        }
+
+        @Override
+        public String toString() {
+            return "Legs{" +
+                    "name='" + name + '\'' +
+                    ", human=" + human +
+                    ", EricEricson=" + EricEricson +
+                    ", Carlsson=" + Carlsson +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Legs legs = (Legs) o;
+            return Objects.equals(name, legs.name) &&
+                    Objects.equals(human, legs.human) &&
+                    Objects.equals(EricEricson, legs.EricEricson) &&
+                    Objects.equals(Carlsson, legs.Carlsson);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, human, EricEricson, Carlsson);
+        }
+    }
+
+    public class Nose {
+        private String name;
+        private Human human;
+        BetanEricsonImpl noseBetanEricson;
+
+        public Nose(String name, Human human) {
+            this.name = name;
+            this.human = human;
+            noseBetanEricson = new BetanEricsonImpl("Бетан");
+            this.setName(name);
+        }
+
+        public void setName(String name) {
+            this.name = name + noseBetanEricson.getName();
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Nose nose = (Nose) o;
+            return Objects.equals(name, nose.name) &&
+                    Objects.equals(human, nose.human) &&
+                    Objects.equals(noseBetanEricson, nose.noseBetanEricson);
+        }
+
+        @Override
+        public String toString() {
+            return "Nose{" +
+                    "name='" + name + '\'' +
+                    ", human=" + human +
+                    ", noseBetanEricson=" + noseBetanEricson +
+                    '}';
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, human, noseBetanEricson);
+        }
+    }
+
 }
