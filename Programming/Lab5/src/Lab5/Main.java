12f693cbe253c7110e32ef393e7e0bd9f0d751b2
Timofey Ermakov <ts.ermakov@yandex.ru>
2021-05-18 11:20:50 +0300

    UML

--- Programming/Lab5/src/Lab5/Main.java
+++ Programming/Lab5/src/Lab5/Main.java
@@ -1,16 +1,28 @@
 package Lab5;
 
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        App app;
+        App app = null;
         try {
-            app = new App(args[0]);
-        } catch (IndexOutOfBoundsException e) {
-            app = new App("");
-        }
-        app.start();
+            byte[] b = new byte[1024];
+            for (int r; (r = System.in.read(b)) != -1; ) {
+                String buffer = new String(b, 0, r);
+                //System.out.println(buffer);
+
+                app = new App(args[0]);
+                app.start();
+            }
+    } catch (Exception e) {
+            System.out.println("Программа завершила выполнение");
+        //app = new App(args[0]);
+        //e.printStackTrace();
+    }
+
     }
 
 }
